c82666eb6d7e4f999881b8b4d2f08e942f78547c
Gabriele Marini <giaga7@gmail.com>
2022-05-04 14:51:21 +1000

    Added dynamic page loading with pushStates

--- js/main.js
+++ js/main.js
@@ -1,61 +1,90 @@
-function generateElements(count, useRandomClass) {
-    console.log("Generating a grid of " + count + " elements!")
-    var containerElement = document.getElementsByClassName("gallery-container")[0];
-    containerElement.innerHTML = "";
-    for (let i = 0; i < count; i++) {
-        var tag = document.createElement("div");
-        tag.classList.add("gallery-element");
-        if (useRandomClass) {
-            var randN = Math.random() * (100 - 1) + 1;
-            if (randN > 66) {
-                tag.classList.add("grow-element");
-            } else if (randN < 33) {
-                tag.classList.add("shrink-element");
+const getData = (pageUrl) => {
+    return new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.onload = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                resolve(this.responseText);
+            } else {
+                reject(this.responseText);
             }
-        }
-        tag.textContent = i + 1;
-        containerElement.append(tag);
-    }
-    document.getElementById("spacing-slider").disabled = false;
-    document.getElementById("spacing-slider-label").removeAttribute("disabled");
-    document.getElementById("spacing-value").removeAttribute("disabled");
-    addOnClicks();
-}
+        };
+        request.open("get", pageUrl, true);
+        request.send();
+    });
+};
 
 
-function onGalleryElementClick(element) {
-    element.classList.toggle("selected");
-    alert("Hey you clicked " + element.textContent);
-}
+function load(container_selector, pageUrl, page_id, page_title, push = true) {
+    console.log("Loading page " + page_id + "from file " + pageUrl + " into container " + container_selector);
+    document.querySelector(container_selector).innerHTML = "";
+    getData(pageUrl)
+        .then((resolve) => {
+            var resp = resolve;
+            if (push)
+                history.pushState({ page: page_id, selector: container_selector, title: page_title, url: pageUrl }, page_title, "?page=" + page_id);
 
-function addOnClicks() {
-    var containerElements = document.getElementsByClassName("gallery-element");
-    for (const element of containerElements) {
-        element.addEventListener("click", function() {
-            onGalleryElementClick(this);
+            document.querySelector(container_selector).innerHTML = resp;
+            document.querySelector(container_selector).style.opacity = 0;
+            setTimeout(function() {
+                document.querySelector(container_selector).style.opacity = 1;
+            }, 200)
+            console.log(pageUrl);
+            console.log(resolve);
+            initializer(page_id)
+        })
+        .catch((reject) => {
+            console.error(reject);
         });
-    }
 }
 
-function onSubmitForm() {
-    console.log("Submitting form and generating grid!")
-    var form = document.getElementById("generate-grid-form");
-    var value = document.getElementById("generate-grid-value");
-    var button = document.getElementById("generate-grid-button");
-    var checkbox = document.getElementById("generate-grid-checkbox");
-    generateElements(value.value, checkbox.checked);
-    return false; // <-- This is to avoid reloading the page when submitting a form!
+function linkClick(event) {
+    console.log("Loading link!");
+    event.preventDefault();
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    loadLink(event.target);
+}
+
+function loadLink(linkElement) {
+    load("#main-container", linkElement.getAttribute("href"), linkElement.getAttribute("page-id"), linkElement.getAttribute("page-title"), true);
+    return false;
+}
+
+window.addEventListener('popstate', (event) => {
+    var url = null;
+
+    if (event.state && event.state.url) {
+        url = event.state.url;
+    } else {
+        url = 'index.html'; // or whatever your initial url was
+    }
+    console.log("Pop state!");
+    console.log("location: " + document.location + ", state: " + JSON.stringify(event.state));
+    load(event.state.selector, event.state.url, event.state.page, event.state.title, false);
+});
+
+
+function initializer(pageId) {
+    switch (pageId) {
+        case "1":
+            document.getElementById("generate-grid").onsubmit = function() {
+                return (onSubmitForm());
+            };
+            initSliders();
+            break;
+        case "2":
+            break;
+        case "page3":
+            break;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    var slider = document.getElementById("spacing-slider");
-    var output = document.getElementById("spacing-value");
-    output.innerHTML = slider.value + "rem"; // Display the default slider value
-
-    // Update the current slider value (each time you drag the slider handle)
-    slider.oninput = function() {
-        output.innerHTML = slider.value + "rem";
-        var containerElement = document.getElementsByClassName("gallery-container")[0];
-        containerElement.style.gap = slider.value + "rem";
+    loadLink(document.getElementById("home-link"));
+    var links = document.getElementsByClassName("nav-link");
+    for (const link of links) {
+        link.addEventListener("click", function(event) {
+            linkClick(event);
+        });
     }
 })
